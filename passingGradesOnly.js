4821fde9e6ff5a0b4398f45c8ae03ec1bb6c7ee3
Amir Mohammed <amir.mohammed00@gmail.com>
2023-04-03 03:22:48 +0000

    complete

---
+++ passingGradesOnly.js
@@ -0,0 +1,10 @@
+const grades = [73, 69, 3, 100, 50, 70, 69, 88, 95, 77, 35];
+
+function passingGrade(grade) {
+}
+
+const filteredGrades2 = grades.filter(function(grade) {
+  return grade >= 70;
+});
+
+console.log(filteredGrades2);
